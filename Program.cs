bfe6884dde49d23c55057f125e0865724473a382
mehmet cantemir <mehmetcantemir81@gmail.com>
2022-12-06 19:29:26 +0300

    Kodluyoruz Arrays Homeworks

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi Tanımlama
+            string[] renkler = new string[5];
+            string[] hayvanlar = { "kedi", "köpek", "kuş", "maymun" };
+            int[] array;
+            array=new int[5];
+
+
+            //Dizilere değer atama ve erişim
+
+            renkler[0] = "Mavi";
+            Console.WriteLine(hayvanlar[0]);
+
+            //Döngülerle dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalaması
+
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz : ");
+            int diziUzunlugu = Convert.ToInt32(Console.ReadLine());
+            int[] sayilar = new int[diziUzunlugu];
+            int toplam = 0;
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0} sayısını giriniz: ",i+1);
+                sayilar[i]=Convert.ToInt32(Console.ReadLine());
+                
+            }
+
+            foreach (var item in sayilar)
+            {
+                toplam += item;
+            }
+
+            int average = toplam / diziUzunlugu;
+            Console.WriteLine("Girilen Sayıların Ortalaması = {0}",average);
+            
+
+
+
+        }
+    }
+}
